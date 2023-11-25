54511d6709287f940f44d6fee6deb3f5a3cb49fe
Lucas Sekwati <Luxturesekwati@gmail.com>
2023-11-24 18:00:53 -0800

    python-object_relational_mapping-update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Lists all cities from the state given as an argument from the database hbtn_0e_4_usa."""
+"""Lists all cities by state from the database hbtn_0e_4_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-        username = sys.argv[1]
-            password = sys.argv[2]
-                database = sys.argv[3]
-                    state_name = sys.argv[4]
-
-                        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-                            cur = db.cursor()
-
-                                cur.execute("""SELECT cities.name FROM
-                                                    cities INNER JOIN states ON states.id=cities.state_id
-                                                                        WHERE BINARY states.name = %s ORDER BY cities.id""", (state_name,))
-                                    rows = cur.fetchall()
-                                        print(", ".join(city[0] for city in rows))
-
-                                            cur.close()
-                                                db.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    state_name = sys.argv[4]
+    cur.execute("SELECT cities.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "WHERE states.name = %s ORDER BY cities.id ASC", (state_name,))
+    rows = cur.fetchall()
+    print(", ".join(row[0] for row in rows))
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all State objects from the database hbtn_0e_6_usa
-"""
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
 
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
 
 if __name__ == "__main__":
-        username = sys.argv[1]
-            password = sys.argv[2]
-                database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-                    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}', pool_pre_ping=True)
-                        
-                            Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database),
+                           pool_pre_ping=True)
 
-                                Session = sessionmaker(bind=engine)
-                                    session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-                                        for state in session.query(State).order_by(State.id).all():
-                                                    print(f"{state.id}: {state.name}")
+    states = session.query(State).order_by(State.id).all()
 
-                                                        session.close()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
