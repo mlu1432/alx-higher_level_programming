2b8c9f08e8b4e6146706ecfefe42988d4a3494e5
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-05-09 18:01:05 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script accepts a URL as input, issues a request to that URL,
 # and outputs the size of the response body.
-curl -sI "$1" | awk '/Content-Length/{print $2}'
+curl -s "$1" -o /dev/null -w '%{size_download}\n'
 
 
