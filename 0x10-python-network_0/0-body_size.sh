1c48f3e4174572ad9d8e8a4cb7982e016fdd57c3
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-05-09 17:58:35 -0700

    body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script accepts a URL as input, issues a request to that URL,
 # and outputs the size of the response body.
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+curl -sI "$1" | awk '/Content-Length/{print $2}'
 
 
