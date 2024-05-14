759e7449ae00faf81d97eaf244a7d0c529e4381c
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-05-14 13:56:52 -0700

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,9 +5,9 @@ sends a POST request to the URL
 with email as parameter, and displays the body of the response decoded in utf-8
 """
 
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
+
 
 if __name__ == "__main__":
     values = {'email': sys.argv[2]}
