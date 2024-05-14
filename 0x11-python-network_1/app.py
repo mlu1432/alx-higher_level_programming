a011a3bf904832b139dca6671daf68b9056b451e
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-05-14 13:51:39 -0700

    update

--- 0x11-python-network_1/app.py
+++ 0x11-python-network_1/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, request
+#!/usr/bin/python3
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/post_email', methods=['POST'])
-def post_email():
-    email = request.form['email']
-    return f'Your email is: {email}'
+@app.route('/')
+def index():
+    return "Index"
 
-if __name__ == '__main__':
+@app.route('/status_401')
+def status_401():
+    return "Unauthorized", 401
+
+@app.route('/doesnt_exist')
+def doesnt_exist():
+    return "Not Found", 404
+
+@app.route('/status_500')
+def status_500():
+    return "Internal Server Error", 500
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
